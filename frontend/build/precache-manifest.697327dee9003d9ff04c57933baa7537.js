25e17c34f5429e68c502c297cc80e97dc0421e5d
Hala959 <alyonbuawi@gmail.com>
2020-05-10 02:14:19 +0300

    navbar

---
+++ frontend/build/precache-manifest.697327dee9003d9ff04c57933baa7537.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba167a61df29d370ba590d37b385ded5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1593a55940765ee9582e",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "12672cb1cc2eff53b909",
+    "url": "/static/js/2.d8ff14ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d8ff14ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1593a55940765ee9582e",
+    "url": "/static/js/main.4076f1f5.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  }
+]);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Signup from './components/user/Signup.js';
+import Login from "./components/user/Login.js";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Navb from "./components/navbar/Navb";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    isAuth: false,
+    user: null, // temp change it to null
+    message: null,
+    isLogin: false,
+    waiting: false,
+  };
+
+ 
+
+  logoutHandler = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token");
+
+    this.setState({
+      isAuth: false,
+      user: null,
+      message: null,
+    });
+  };
+
+  render() {
+    return (
+      <div className ="app">
+        <Navb  />
+<Switch>
+        <Route path="/signup" component={Signup} />} />
+          <Route
+            path="/login"
+            render={(props) => (
+              <Login
+                {...props}
+                authLogin={this.authLogin}
+                userLogin={this.userLogin}
+              />
+            )}
+          />
+          </Switch>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
