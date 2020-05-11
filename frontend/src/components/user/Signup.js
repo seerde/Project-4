cfe97803fd217a5197d9fe4befaa2878ee6f68eb
ibrahimvis <ibrahim.a7md@gmail.com>
2020-05-11 23:57:48 +0300

    login signup and routes

--- frontend/src/components/user/Signup.js
+++ frontend/src/components/user/Signup.js
@@ -1,53 +1,66 @@
-import React, { Component } from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
- class Signup extends Component {
+const Signup = (props) => {
 
-    constructor(props){
-    super(props);
-    this.state={
-        
-     };
-   }
-   submitSignup (e){
+    const [register, setRegister] = useState({});
 
-
-
-   }
-   render() {
-            return (
-              <div className ="container">
-            <div className ="inner-container-m">
-                <div className= "box">
-
-                    <div className="input-group">
-                  
-                        <input type ="text" name="username " placeholder ="Username"/>
-                    </div>
-                   
-                    <div className="input-group">
-                        <label htmlFor ="emil"></label>
-                        <input type ="text" name="Email"  placeholder ="Email Address "/>
-                    </div>
-
-                    <div className="input-group">
-                        <label htmlFor ="password"></label>
-                        <input type ="password" name="password"  placeholder ="Password"/>
-                    </div>
-
-                    <div className="input-group">
-                    <label htmlFor ="password"></label>
-                        <input type ="password" name="Password confirmation "  placeholder ="Password confirmation "/>
-                    </div>
-
-
-                    <button type ="button" className="btn btn-sm" onClick={this.submitSignup.bind(this)} >Sign Up</button>
-                </div>
-                
-            </div>
-            </div>
-            )
+    let changeHandler = ({ target: { name, value } }) => {
+        setRegister({ ...register, [name]: value });
+        // console.log(name, value)
     }
+
+    let registerHandler = async (e) => {
+        e.preventDefault();
+        // console.log(register["password_confirmation"])
+        if (register["password"] == register["password_confirmation"]) {
+            try {
+                let data = await axios.post("http://localhost:3001/api/auth/signup", register);
+                if (data.data.token) {
+                    localStorage.setItem("token", data.data.token);
+                    props.userLogin(data.data.token);
+                    props.history.push("/");
+                } else throw { message: "something" }
+            } catch (err) {
+                console.log(err);
+            }
+        } else {
+            console.log("password confor is bla bla bla")
+            //show error message 
+        }
+    }
+
+    return (
+        <div className="container">
+            <div className="inner-container-m">
+                <div className="box">
+
+                    <div className="input-group">
+                        <input type="text" name="username" placeholder="Username" onChange={changeHandler} />
+                    </div>
+
+                    <div className="input-group">
+                        <label htmlFor="emil"></label>
+                        <input type="email" name="email" placeholder="Email Address " onChange={changeHandler} />
+                    </div>
+
+                    <div className="input-group">
+                        <label htmlFor="password"></label>
+                        <input type="password" name="password" placeholder="Password" onChange={changeHandler} />
+                    </div>
+
+                    <div className="input-group">
+                        <label htmlFor="password"></label>
+                        <input type="password" name="password_confirmation" placeholder="Password confirmation " onChange={changeHandler} />
+                    </div>
+
+
+                    <button type="button" className="btn btn-sm" onClick={(e) => registerHandler(e)} >Sign Up</button>
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
 export default Signup;
- 
