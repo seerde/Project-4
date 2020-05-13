f1f506e04aae659eaf406de2d78795df3903e544
Ibtihal Faris <alotaibi.ibtihal@gmail.com>
2020-05-13 05:48:55 +0300

    added the profile

--- frontend/src/components/home/Profile.js
+++ frontend/src/components/home/Profile.js
@@ -1,41 +1,38 @@
 import React from 'react'
+import { Container, Row, Col, Image } from "react-bootstrap";
 
 const Profile = () => {
     return (
         <div className="inner-profile-container">
-      <div className="box">
-        <div className="input-group">
-          <label htmlFor="emil"></label>
-          <input
-            type="text"
-            name="email"
-            className="login-input input-xlarge"
-            placeholder="Email Address"
-           
-          />
-        </div>
 
-        <div className="input-group">
-          <label htmlFor="password"></label>
-          <input
-            type="password"
-            name="password"
-            className="login-input"
-            placeholder="Password"
-          
-          />
+            <div className="input-group">
+                <Container className="mt-5">
+                    <Row>
+                        <Image className="rounded mx-auto d-block" src="https://via.placeholder.com/150" rounded />
+                    </Row>
+                    <Row>
+                        <label className="text-light mx-auto">User Name</label>
+                    </Row>
+                    <Row className="text-light text-center text-uppercase mt-5">
+                        <Col xs={6}>
+                        <label>Overall</label>
+                        </Col>
+                        <Col xs={6}>
+                        <label>Feedback</label>
+                        </Col>
+                    </Row>
+                    <Row className="text-light text-center mt-3">
+                        <Col xs={6}>
+                        <span class="border border-primary p-2"> 0 </span>
+                        </Col>
+                        <Col xs={6}>
+                        <span class="border border-primary p-2"> 0 </span>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+
         </div>
-        <div className="form-group text-center">
-          <button
-            type="button"
-            className="btn btn-success btn-lg"
-            
-          >
-            Login
-          </button>
-        </div>
-      </div>
-    </div>
     )
 }
 
