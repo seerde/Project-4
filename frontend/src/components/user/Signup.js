91dfa182a31eabc86260a63853a84a3daa6f012e
ibrahimvis <ibrahim.a7md@gmail.com>
2020-05-14 04:30:46 +0300

    adding alert messages for signing up errors and login errors

--- frontend/src/components/user/Signup.js
+++ frontend/src/components/user/Signup.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { Alert } from "react-bootstrap";
 
 const Signup = (props) => {
   const [register, setRegister] = useState({});
+  const [isSignedUp, setIsSignedUp] = useState({ isSigned: false, message: "", variant: "danger" });
 
   let changeHandler = ({ target: { name, value } }) => {
     setRegister({ ...register, [name]: value });
-    // console.log(name, value)
   };
 
   let registerHandler = async (e) => {
@@ -14,72 +15,84 @@ const Signup = (props) => {
     // console.log(register["password_confirmation"])
     if (register["password"] == register["password_confirmation"]) {
       try {
-        let data = await axios.post("/api/auth/signup", register);
+        let data = await axios.post("http://localhost:3005/api/auth/signup", register);
         if (data.data.token) {
           localStorage.setItem("token", data.data.token);
           props.userLogin(data.data.token);
           props.history.push("/");
         } else throw { message: "something" };
       } catch (err) {
-        console.log(err);
+        setIsSignedUp({ isSigned: true, message: "The Email or the username already exsits", variant: "danger" });
+        setTimeout(() => {
+          setIsSignedUp({ isSigned: false, message: "" });
+        }, 4000);
       }
     } else {
-      console.log("password confor is bla bla bla");
-      //show error message
+      setIsSignedUp({ isSigned: true, message: "Password doesn't match", variant: "danger" });
+      setTimeout(() => {
+        setIsSignedUp({ isSigned: false, message: "" })
+      }, 4000);
     }
   };
 
   return (
-    <div className="inner-profile-container">
-      <div className="box">
-        <div className="input-group">
-          <input
-            type="text"
-            name="username"
-            placeholder="Username"
-            onChange={changeHandler}
-          />
-        </div>
+    <>
+      {isSignedUp.isSigned && (
+        <Alert variant={isSignedUp.variant}>
+          {isSignedUp.message}
+        </Alert>
+      )}
+      <div className="inner-profile-container">
+        <div className="box">
+          <div className="input-group">
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              onChange={changeHandler}
+            />
+          </div>
 
-        <div className="input-group">
-          <label htmlFor="emil"></label>
-          <input
-            type="email"
-            name="email"
-            placeholder="Email Address "
-            onChange={changeHandler}
-          />
-        </div>
+          <div className="input-group">
+            <label htmlFor="emil"></label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email Address "
+              onChange={changeHandler}
+            />
+          </div>
 
-        <div className="input-group">
-          <label htmlFor="password"></label>
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            onChange={changeHandler}
-          />
-        </div>
+          <div className="input-group">
+            <label htmlFor="password"></label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={changeHandler}
+            />
+          </div>
 
-        <div className="input-group">
-          <label htmlFor="password"></label>
-          <input
-            type="password"
-            name="password_confirmation"
-            placeholder="Password confirmation "
-            onChange={changeHandler}
-          />
-        </div>
+          <div className="input-group">
+            <label htmlFor="password"></label>
+            <input
+              type="password"
+              name="password_confirmation"
+              placeholder="Password confirmation "
+              onChange={changeHandler}
+            />
+          </div>
 
-        <button
-          type="button"
-          className="btn btn-sm"
-          onClick={(e) => registerHandler(e)}
-        >
-          Sign Up
+          <button
+            type="button"
+            className="btn btn-sm"
+            onClick={(e) => registerHandler(e)}
+          >
+            Sign Up
         </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
